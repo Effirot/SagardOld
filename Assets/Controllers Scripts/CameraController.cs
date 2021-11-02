4ab7163b5d7505af86b45aaa60d5067be5f56a1d
Vezyang <vezyang@gmail.com>
2021-11-02 22:48:15 +0500

    cameraFix

--- Assets/Controllers Scripts/CameraController.cs
+++ Assets/Controllers Scripts/CameraController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float CameraSPDZ = 20, CameraSPDX = 20, rotSpeed = 1;
+    public Transform Camera;
 
     void Start()
     {
+
     }
 
     void Update()
     {
-        transform.eulerAngles += new Vector3(0, Input.GetAxis("Rotor") * rotSpeed, 0);
-        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * CameraSPDX, 0, Input.GetAxis("Vertical") * Time.deltaTime * CameraSPDZ);
+        transform.Rotate(0, Input.GetAxis("Rotor") * rotSpeed, 0);
+        Camera.transform.localPosition += new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * CameraSPDX, 0, Input.GetAxis("Vertical") * Time.deltaTime * CameraSPDZ);
     }
 }
