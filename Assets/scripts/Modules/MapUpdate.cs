e6bb0351d2d3af875a1da4d8fa3fd1f020824ffa
Effirot <maximus323i@mail.ru>
2022-06-03 18:27:05 +0500

    main

--- Assets/scripts/Modules/MapUpdate.cs
+++ Assets/scripts/Modules/MapUpdate.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using UnityEditor.SceneManagement;
 
+public class MapUpdate : MonoBehaviour
+{
+    
+}
